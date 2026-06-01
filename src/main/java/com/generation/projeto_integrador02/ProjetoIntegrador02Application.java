033a0b65db0a6aa910a8a88024de8afb38eba698
Renan <renanfcunha@gmail.com>
2026-06-01 10:43:42 -0300

    Projeto e Repositório inicializados

---
+++ src/main/java/com/generation/projeto_integrador02/ProjetoIntegrador02Application.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_integrador02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrador02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrador02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projeto_integrador02/ProjetoIntegrador02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_integrador02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegrador02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
