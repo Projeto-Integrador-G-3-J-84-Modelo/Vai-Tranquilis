a9afb0fcfd80376cbe1986609f8c6cf2d4f1b096
Jessica Aquino <aquinolobobr1811@gmail.com>
2026-06-01 15:07:09 -0300

    Adicionado os relacionamentos entre as entidades

--- src/main/java/com/generation/projeto_integrador02/model/PlanoSeguro.java
+++ src/main/java/com/generation/projeto_integrador02/model/PlanoSeguro.java
@@ -1,9 +1,16 @@
 package com.generation.projeto_integrador02.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -26,6 +33,50 @@ public class PlanoSeguro {
 	
 	@NotNull
 	private Double coberturaMaxima;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "plano", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties(value = "plano", allowSetters = true)
+    private List<PlanoSeguro> plano;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomePlano() {
+		return nomePlano;
+	}
+
+	public void setNomePlano(String nomePlano) {
+		this.nomePlano = nomePlano;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getCoberturaMaxima() {
+		return coberturaMaxima;
+	}
+
+	public void setCoberturaMaxima(Double coberturaMaxima) {
+		this.coberturaMaxima = coberturaMaxima;
+	}
+
+	public List<PlanoSeguro> getPlano() {
+		return plano;
+	}
+
+	public void setPlano(List<PlanoSeguro> plano) {
+		this.plano = plano;
+	}
 	
 	
 }
--- src/main/java/com/generation/projeto_integrador02/model/Usuario.java
+++ src/main/java/com/generation/projeto_integrador02/model/Usuario.java
@@ -1,9 +1,16 @@
 package com.generation.projeto_integrador02.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,11 +22,11 @@ public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank
 	@Size(max = 120, message = "O atributo nome deve conter no máximo 120 caracteres")
 	private String nome;
-	
+
 	@NotBlank
 	@Size(max = 120, message = "O atributo e-mail deve conter no máximo 120 caracteres")
 	private String email;
@@ -27,8 +34,53 @@ public class Usuario {
 	@NotBlank
 	@Size(max = 100, message = "O atributo foto deve conter no máximo 100 caracteres")
 	private String foto;
-	
+
 	@NotBlank
 	@Size(max = 50, message = "O atributo senha deve conter no máximo 50 caracteres")
 	private String senha;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties(value = "usuario", allowSetters = true)
+	private List<SeguroVida> seguro;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
