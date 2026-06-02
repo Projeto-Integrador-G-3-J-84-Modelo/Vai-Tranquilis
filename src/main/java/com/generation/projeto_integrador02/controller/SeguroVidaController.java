f2a7808dbc6a5189ded38a14f6421a55acae668e
luana <luanamporto56@gmail.com>
2026-06-02 09:36:56 -0300

    Finalizando a classe Service e Controller

---
+++ src/main/java/com/generation/projeto_integrador02/controller/SeguroVidaController.java
@@ -0,0 +1,57 @@
+package com.generation.projeto_integrador02.controller;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.generation.projeto_integrador02.model.SeguroVida;
+import com.generation.projeto_integrador02.service.SeguroVidaService;
+
+@RestController
+@RequestMapping("/seguros")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class SeguroVidaController {
+
+    @Autowired
+    private SeguroVidaService seguroVidaService;
+
+    @GetMapping
+    public ResponseEntity<List<SeguroVida>> getAll() {
+        return ResponseEntity.ok(seguroVidaService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SeguroVida> getById(@PathVariable Long id) {
+        return seguroVidaService.buscarPorId(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<SeguroVida> post(@Valid @RequestBody SeguroVida seguro) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(seguroVidaService.criar(seguro));
+    }
+
+    @PutMapping
+    public ResponseEntity<SeguroVida> put(@Valid @RequestBody SeguroVida seguro) {
+        return ResponseEntity.ok(seguroVidaService.atualizar(seguro));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        seguroVidaService.deletar(id);
+    }
+
+    // ENDPOINT
+    @GetMapping("/elegibilidade/{id}")
+    public ResponseEntity<Map<String, String>> checarElegibilidade(@PathVariable Long id) {
+        String mensagem = seguroVidaService.verificarElegibilidade(id);
+        
+        return ResponseEntity.ok(Collections.singletonMap("mensagem", mensagem));
+    }
+}
